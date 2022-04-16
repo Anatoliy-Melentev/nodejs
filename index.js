c4066cfa39a75c572dc883f0c2c2884f04c4bae2
Anatoliy <muzhik.lapotnik@gmail.com>
2022-04-16 11:20:27 +0500

    Домашка 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const { green, red, yellow } = require("colors/safe");
+const process = require('process');
+
+const isPrime = n => n > 1 && ![...Array(n).keys()].find(v => v > 1 && !(n % v));
+const getPrime = (from, to) => [...Array(+to + 1).keys()].filter(value => +from <= value && isPrime(value));
+const getColor = (val, idx) => !((idx - 1) % 3) ? yellow(val) : (!((idx + 1) % 3) ? red(val) : green(val));
+
+const [,,from,to] = process.argv;
+
+console.log(
+  isNaN(+from) || isNaN(+to)
+    ? red('Ошибка, аргумент, переданный при запуске, не считается числом!')
+    : getPrime(from, to).map(getColor).join(' ') || red('Простых чисел в диапазоне нет')
+);
